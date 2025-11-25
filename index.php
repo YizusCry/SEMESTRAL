1e52bdc4165aab356bd74e93233054faafc06c90
EduardoGG2 <129633786+EduardoGG2@users.noreply.github.com>
2025-11-25 04:27:05 +0000

    14-24

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 
 // Si NO hay sesión → login
 if (!isset($_SESSION["usuario_id"])) {
-    header("Location: public/js/login.html");
+    header("Location: publics/js/login.html");
     exit;
 }
 
